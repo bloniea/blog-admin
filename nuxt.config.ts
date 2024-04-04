0c5a0db005ca3455eec7884d3715c07557dbf862
bloniea <bloniea@qq.com>
2024-04-04 16:32:48 +0800

    引入element 样式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   devtools: { enabled: true },
-  modules: ["@element-plus/nuxt", "@pinia/nuxt"],
+  modules: ["@element-plus/nuxt"],
   pages: true,
   app: {
     head: {
       htmlAttrs: { class: "dark" },
     },
   },
-  css: ["~/assets/css/main.styl", "~/assets/css/app.css"],
+  css: [
+    "~/assets/css/main.styl",
+    "~/assets/css/app.css",
+    "element-plus/theme-chalk/index.css",
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/scss/element/index.scss" as element;`,
+        },
+      },
+    },
+  },
+  elementPlus: {
+    icon: "ElIcon",
+    importStyle: "scss",
+    themes: ["dark"],
   },
   nitro: {
     devProxy: {
